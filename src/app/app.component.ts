ef308959639074444c6fa3d0f4f5ae9c84bb5b65
MoleiroClase <alberto.moleiro7@gmail.com>
2022-11-25 19:34:52 +0100

    Form alta empleados done, next day work: disable button if inputs are clear, dont allow duplicated employees

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { Empleado } from 'src/models/empleado.modelo';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AltaEmpleados';
+  title:string = 'AltaEmpleados';
+  empleados:Array<Empleado>=[];
+  salario:number=0;
+  
+  agregarEmpleado(nombre:string,apellido:string,cargo:string,salario:string){
+    this.salario=Number(salario);
+    this.empleados.push(new Empleado(nombre,apellido,cargo,this.salario));
+  }
+
 }
---
+++ src/models/empleado.modelo.ts
@@ -0,0 +1,15 @@
+export class Empleado{
+    public static idS:number=1;
+    public nombre:string="";
+    public apellido:string="";
+    public cargo:string="";
+    public salario:number=0;
+    public id:number=Empleado.idS;
+    constructor(nombre:string,apellido:string,cargo:string,salario:number){
+        this.id=Empleado.idS++;
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.cargo=cargo;
+        this.salario=salario;
+    }
+}
